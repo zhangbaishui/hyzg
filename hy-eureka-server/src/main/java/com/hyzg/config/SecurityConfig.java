e7960029c1a6591096e73ed9d68b5563b1ace3f0
张泉 <5308591+zhangbaishui@user.noreply.gitee.com>
2019-11-19 19:04:13 +0800

    化运中国项目框架

---
+++ hy-eureka-server/src/main/java/com/hyzg/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.hyzg.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //对于这个接口直接忽略
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
---
+++ hy-zuul-server/src/main/java/com/hyzg/ZuulServerApplication.java
@@ -0,0 +1,35 @@
+package com.hyzg;//
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//                       .::::.
+//                     .::::::::.
+//                    ::::::::::: 
+//                 ..:::::::::::'
+//              '::::::::::::'
+//                .::::::::::
+//           '::::::::::::::..
+//                ..::::::::::::.
+//              ``::::::::::::::::
+//               ::::``:::::::::'        .:::.
+//              ::::'   ':::::'       .::::::::.
+//            .::::'      ::::     .:::::::'::::.
+//           .:::'       :::::  .:::::::::' ':::::.
+//          .::'        :::::.:::::::::'      ':::::.
+//         .::'         ::::::::::::::'         ``::::.
+//     ...:::           ::::::::::::'              ``::.
+//    ```` ':.          ':::::::::'                  ::::..
+//                       '.:::::'                    ':'````..
+//
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class,args);
+    }
+}
